549868dadbb442bd6730e02d1677a3cfdcadc91a
Nick Mours <nickxylanmelloria@gmail.com>
2025-01-05 08:28:33 +0800

    basic setup: code in client, output from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,36 @@
+import Editor from '@monaco-editor/react';
+import { Button } from './components/ui/button';
+import { api } from './api/axios';
+import { useState } from 'react';
+
 function App() {
+	const [data, setData] = useState<string | undefined>(undefined);
+
+	const onChange = (value: string | undefined) => {
+		setData(value);
+	};
+
+	const handleSubmit = async () => {
+		try {
+			const { data: result } = await api.post('/', { data: data });
+
+			console.log(result);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<main>
 			<h1 className='text-red-500'>Hello World!</h1>
+			<Editor
+				height='90vh'
+				defaultLanguage='javascript'
+				theme='vs-dark'
+				defaultValue='// some comment \n\n const x = 1;\n console.log(x);'
+				onChange={onChange}
+			/>
+			<Button onClick={handleSubmit}>Submit</Button>
 		</main>
 	);
 }
